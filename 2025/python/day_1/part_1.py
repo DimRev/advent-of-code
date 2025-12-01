cadc9728c48ad4d15cb4b37d58625dfea616183e
DimRev <dimarev444@gmail.com>
2025-12-01 19:22:27 +0200

    day1(python): Add solutions for d1p1 and p2 python

--- 2025/python/day_1/part_1.py
+++ 2025/python/day_1/part_1.py
@@ -1,2 +1,35 @@
+import sys
+from typing import Tuple
+FILE_PATH = '../inputs/day-1/part-1/input.txt'
+
+def parseLine(line: str)-> Tuple[str, int]:
+    if len(line) < 2:
+        return ""
+
+    return line[0], int(line[1:])
+
 def day1_part1():
-    print("Hello from Day 1, part 1")
+    try:
+        with open(FILE_PATH, 'r') as f:
+            n = 50
+            z = 0
+            for line in f:
+
+                direction, dist = parseLine(line.strip())
+
+                if direction == 'L':
+                    n -= dist
+                else:
+                    n += dist
+
+                n = ((n % 100) + 100) % 100
+                if n == 0:
+                    z += 1
+
+            print(f'Solution: {z}')
+    except FileNotFoundError:
+        print('File not found')
+        sys.exit(1)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
--- 2025/python/day_1/part_2.py
+++ 2025/python/day_1/part_2.py
@@ -1,2 +1,60 @@
+import sys
+from typing import Tuple
+FILE_PATH = '../inputs/day-1/part-1/input.txt'
+
+def parseLine(line: str)-> Tuple[str, int]:
+    if len(line) < 2:
+        return ""
+
+    return line[0], int(line[1:])
+
+
+def calculatePassesOverZero(dir: str, start_pos: int, distance: int) -> int:
+    if distance == 0:
+        return 0
+
+    if dir == 'R':
+        distance_to_zero = 100 - start_pos
+        if start_pos == 0:
+            distance_to_zero = 100
+        if distance >= distance_to_zero:
+            return 1 + (distance - distance_to_zero) // 100
+        return 0
+    else:
+        distance_to_zero = start_pos
+        if start_pos == 0:
+            distance_to_zero = 100
+        if distance >= distance_to_zero:
+            return 1 + (distance - distance_to_zero) // 100
+        return 0
+
 def day1_part2():
-    print("Hello from Day 1, part 2")
+    try:
+        with open(FILE_PATH, 'r') as f:
+            n = 50
+            z = 0
+            for line in f:
+
+                direction, dist = parseLine(line.strip())
+
+                full_rotations_from_dist = dist // 100
+                z += full_rotations_from_dist
+                dist = dist % 100
+
+                passes_over_zero = calculatePassesOverZero(direction, n, dist)
+                z += passes_over_zero
+
+                if direction == 'L':
+                    n -= dist
+                else:
+                    n += dist
+
+                n = ((n % 100) + 100) % 100
+
+            print(f'Solution: {z}')
+    except FileNotFoundError:
+        print('File not found')
+        sys.exit(1)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
