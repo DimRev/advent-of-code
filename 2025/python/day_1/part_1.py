37ba09e982d2ae24dfba46e4f8452b692d07eba3
DimRev <dimarev444@gmail.com>
2025-12-01 19:22:27 +0200

    wip(day-1-python): Working on solution

--- 2025/python/day_1/part_1.py
+++ 2025/python/day_1/part_1.py
@@ -1,2 +1,23 @@
+import sys
+from typing import Tuple
+FILE_PATH = '../inputs/day-1/part-1/example.txt'
+
+def parseLine(line: str)-> Tuple[str, int]:
+    if len(line) < 2:
+        return ""
+
+    return line[0], int(line[1:])
+
 def day1_part1():
-    print("Hello from Day 1, part 1")
+    try:
+        with open(FILE_PATH, 'r') as f:
+            n = 50
+            for line in f:
+
+                direction, num = parseLine(line.strip())
+    except FileNotFoundError:
+        print('File not found')
+        sys.exit(1)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
